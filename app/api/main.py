15ddcf6a32be322550048b6f2649db2f8e4a50be
kob22 <kob2222@gmail.com>
2020-02-14 16:10:08 +0100

    text scraping

--- app/api/main.py
+++ app/api/main.py
@@ -3,8 +3,8 @@ from app import mongo
 from flask import jsonify
 from bson import json_util
 from app.api.helpers import validate_url
-from celery_app.task_receiver import scrap_image
-
+from celery_app.task_receiver import scrap_image, scrap_text
+from bson.objectid import ObjectId
 import datetime
 
 
@@ -39,24 +39,70 @@ def web_images_download(url):
 
     # try ?
     post_id = mongo.db.images.insert_one(image).inserted_id
-    scrap_image.delay(url)
+    scrap_image.delay(url, str(post_id))
 
     return str(post_id), 201
 
 
-@bp.route('/text/<path:url>', methods=['GET'])
-@validate_url
-def web_text_status(url):
+@bp.route('/text/status/<path:data>', methods=['GET'])
+def web_text_status(data):
     """Get status of the text download request"""
 
-    return 'text' + url
+    if '/' in data:
+        web_text = mongo.db.web_text.find({"url": data}, {"url":1, "status": 1, "_id":0}).sort("date", -1).limit(1)
+    else:
+        web_text = mongo.db.web_text.find({'_id': ObjectId(data)}, {"url":1, "status": 1, "_id":0})
+
+    if web_text.count() > 0:
+        return json_util.dumps(web_text)
+    else:
+        return jsonify({"error": "URL or ID not found"}), 400
 
 
-@bp.route('/text/<path:url>', methods=['POST'])
+@bp.route('/text/download/<path:data>', methods=['GET'])
+def web_text_download(data):
+    """Download text from saved website"""
+
+    if '/' in data:
+        web_text = mongo.db.web_text.find({"url": data, }, {"url":1, "status": 1, "_id":0}).sort("date", -1).limit(1)
+    else:
+        web_text = mongo.db.web_text.find({'_id': ObjectId(data)}, {"url":1, "status": 1, "_id":0})
+
+    if web_text.count() > 0:
+        for txt in web_text:
+            if txt['status'] == 'FINISHED':
+                return json_util.dumps(txt)
+            elif txt['status'] != 'FAILED':
+                return jsonify({"error": "NOT READY"}), 400
+            else:
+                return jsonify({"error": "FAILED, Please Retry"}), 400
+    else:
+        return jsonify({"error": "URL or ID not found"}), 400
+
+
+@bp.route('/text/all', methods=['GET'])
+def web_text_all():
+    """Shows all queries"""
+    web_text = mongo.db.web_text.find()
+
+    return json_util.dumps(web_text)
+
+
+@bp.route('/text/scrap/<path:url>', methods=['POST'])
 @validate_url
-def web_text_download(url):
+def web_text_scrap(url):
     """Text download request"""
+    web_text = {
+        "url": url,
+        "status": "Accepted",
+        "text": "",
+        "date": datetime.datetime.utcnow()
+    }
 
-    return 'post text' + url
+    post_id = mongo.db.web_text.insert_one(web_text).inserted_id
+
+    scrap_text.delay(url, str(post_id))
+
+    return 'post text' + url, 201
 
 
--- celery_app/task_receiver.py
+++ celery_app/task_receiver.py
@@ -1,11 +1,56 @@
 
 from celery_app.celery_app import app
 import logging
+import requests
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+from config import Config
+from bson.objectid import ObjectId
+import re
+
+
+def on_fail(self, exc, task_id, args, kwargs, einfo):
+    update_status(args[1], 'FAILED')
+
+
+def on_retry(self, exc, task_id, args, kwargs, einfo):
+    update_status(args[1], 'RETRY')
 
 
 # auto retry for exception
-@app.task(bind=True, default_retry_delay=30, autoretry_for=(Exception,), max_retries = 5)
-def scrap_image(self, url):
+@app.task(bind=True, default_retry_delay=30, autoretry_for=(Exception,), max_retries=5)
+def scrap_image(self, url, UID):
     logging.info(f'Got url {url}')
 
     return 10
+
+
+@app.task(bind=True, default_retry_delay=30, autoretry_for=(Exception,), max_retries=5, on_failure=on_fail, on_retry=on_retry)
+def scrap_text(self, url, UID):
+    logging.info(f'Got url {url}')
+    update_status(UID, "Started")
+
+    web_html = requests.get(url).text
+    page_text = BeautifulSoup(web_html, "lxml")
+
+    # remove js and style from text
+    for script in page_text(["script", "style"]):
+        script.decompose()
+    text = page_text.get_text()
+    text = re.sub("[ \n\r\t]{2,}", "\n", text)
+
+    with MongoClient(Config.MONGO_URI) as mongo:
+        mongo.db.web_text.find_one_and_update(
+            {'_id': ObjectId(UID)}, {"$set": {'text': text, 'status': "FINISHED"}}
+        )
+    return 0
+
+
+def update_status(UID, status):
+    """Update status"""
+    with MongoClient(Config.MONGO_URI) as mongo:
+        mongo.db.web_text.find_one_and_update(
+            {'_id': ObjectId(UID)}, {"$set": {'status': status}}
+        )
+
+
